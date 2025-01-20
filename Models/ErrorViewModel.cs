c7ddb09a7fd59b63f84d76c56dbd74d3aa7d7a8c
seydakurtdere <s.kurtdere@gmail.com>
2025-01-20 22:43:45 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Paintolog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
